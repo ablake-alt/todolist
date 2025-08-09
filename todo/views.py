2fd2fb636fc9e5fc11482d4cca89957e93ed7b21
ablake-alt <adbleke89@outlook.com>
2025-08-09 11:01:34 -0400

    first commit

---
+++ todo/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todo/index.html', context)
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return redirect('/')
+
+def toggle_task(request, pk):
+    task = Task.objects.get(id=pk)
+    task.completed = not task.completed  # Flip True/False
+    task.save()
+    return redirect('/')
